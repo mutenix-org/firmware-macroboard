4d460b37cc501899fe28b1fe7ec81b66a7d9ccc0
Matthias Bilger <matthias@bilger.info>
2025-01-14 22:49:42 +0100

    chore: add bluetooth library

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -47,4 +47,7 @@ OUTPUT_FILE="release/${VERSION}.tar.gz"
 # Create a tar.gz archive containing all files in src/mutenix_firmware
 tar -czvf "$OUTPUT_FILE" -C src/mutenix_firmware .
 
+# Add the lib folder to the tar.gz archive
+tar --append -f "$OUTPUT_FILE" -C . lib
+
 echo "Release package created: $OUTPUT_FILE"
